6462a82a8b1cb51bc9cfaa0fd56a28e69804315e
IvanBlacky <tcherivan@gmail.com>
2019-03-05 19:55:34 +0300

    #50 Feat: add iframely endpoint

---
+++ src/controllers/Iframely.js
@@ -0,0 +1,61 @@
+const fetch = require('node-fetch');
+const core = require('gls-core-service');
+const Basic = core.controllers.Basic;
+const Logger = core.utils.Logger;
+const env = require('../data/env');
+
+class Iframely extends Basic {
+    async getEmbed({ params: { type, url } }) {
+        if (!this._isValidType(type)) {
+            throw {
+                code: 1101,
+                message: `Type ${type} is not a valid type`,
+            };
+        }
+
+        if (!this._isValidUrl(url)) {
+            throw {
+                code: 1101,
+                message: `URL ${url} is not a valid URL`,
+            };
+        }
+
+        const embedUrl = `${env.GLS_IFRAMELY_CONNECT}/${type}?url=${url}`;
+
+        try {
+            const response = await fetch(embedUrl);
+            if (response.ok) {
+                return await response.json();
+            } else {
+                throw {
+                    code: 1102,
+                    message: 'Iframely error',
+                    error: await response.text(),
+                };
+            }
+        } catch (error) {
+            Logger.error('Iframely error -- ', error.stack || error);
+            throw error;
+        }
+    }
+
+    _isValidType(type) {
+        return ['oembed', 'iframely'].indexOf(type) === -1 ? false : true;
+    }
+
+    _isValidUrl(url) {
+        const pattern = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+        /* const pattern = new RegExp(
+            '^(https?:\\/\\/)?' + // protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+                '(\\#[-a-z\\d_]*)?$', // fragment locator
+            'i'
+        ); */
+        return !!pattern.test(url);
+    }
+}
+
+module.exports = Iframely;

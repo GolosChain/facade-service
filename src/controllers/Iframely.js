3b0a7a1e608912df9c1d05e3f8d0f0870240004c
IvanBlacky <tcherivan@gmail.com>
2019-04-03 17:20:47 +0300

    #66 Refactor: improve url validation

--- src/controllers/Iframely.js
+++ src/controllers/Iframely.js
@@ -1,9 +1,9 @@
 const fetch = require('node-fetch');
+const urlValidator = require('valid-url');
 const core = require('gls-core-service');
 const Basic = core.controllers.Basic;
 const Logger = core.utils.Logger;
 const env = require('../data/env');
-const URL_PATTERN = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-zA-Z\u00a1-\uffff0-9]-*)*[a-zA-Z\u00a1-\uffff0-9]+)(?:\.(?:[a-zA-Z\u00a1-\uffff0-9]-*)*[a-zA-Z\u00a1-\uffff0-9]+)*(?:\.(?:[A-Za-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
 
 class Iframely extends Basic {
     async getEmbed({ params: { type, url } }) {
@@ -41,7 +41,7 @@ class Iframely extends Basic {
     }
 
     _validateUrlOrThrow(url) {
-        const urlIsValid = URL_PATTERN.test(url);
+        const urlIsValid = urlValidator.isWebUri(url);
 
         if (!urlIsValid) {
             throw {

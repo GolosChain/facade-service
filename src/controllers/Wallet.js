eaa71c19b2dbdff61f3a2c08c9fe0f3313d85b2f
IvanBlacky <tcherivan@gmail.com>
2019-07-12 18:06:20 +0300

    Hotfix: add missing methods

--- src/controllers/Wallet.js
+++ src/controllers/Wallet.js
@@ -5,21 +5,35 @@ class Wallet extends Basic {
     async getBalance({ params }) {
         return await this.callService('wallet', 'getBalance', params);
     }
+    
     async getDelegationState({ params }) {
         return await this.callService('wallet', 'getDelegationState', params);
     }
+    
     async getTransferHistory({ params }) {
-        return await this.callService('wallet', 'getHistory', params);
+        return await this.callService('wallet', 'getTransferHistory', params);
     }
+    
+    async getRewardsHistory({ params }) {
+        return await this.callService('wallet', 'getRewardsHistory', params);
+    }
+    
+    async getVestingHistory({ params }) {
+        return await this.callService('wallet', 'getVestingHistory', params);
+    }
+    
     async getTokensInfo({ params }) {
         return await this.callService('wallet', 'getTokensInfo', params);
     }
+    
     async getVestingInfo({ params }) {
         return await this.callService('wallet', 'getVestingInfo', params);
     }
+    
     async convertVestingToToken({ params }) {
         return await this.callService('wallet', 'convertVestingToToken', params);
     }
+    
     async convertTokensToVesting({ params }) {
         return await this.callService('wallet', 'convertTokensToVesting', params);
     }

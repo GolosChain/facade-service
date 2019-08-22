86e95eb4b12d66f2ee36371ead3487a77588f77c
IvanBlacky <tcherivan@gmail.com>
2019-08-22 13:52:03 +0300

    Feat: add find leaders

--- src/services/Connector.js
+++ src/services/Connector.js
@@ -357,6 +357,10 @@ class Connector extends BasicConnector {
                     handler: content.getLeadersTop,
                     scope: content,
                 },
+                'content.findLeaders': {
+                    handler: content.findLeaders,
+                    scope: content,
+                },
                 'content.getHashTagTop': {
                     handler: content.getHashTagTop,
                     scope: content,
